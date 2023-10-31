e6e3b83d4a0ff4d4fc91de7ad41137383a915233
Enver <mr_epsty@hotmail.com>
2023-10-31 16:30:51 +1100

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    return Math.abs(blocks - 42);
+}
+
+function distanceFromHqInFeet(feet) {
+    return distanceFromHqInBlocks(feet)*264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs((start - destination)*264);
+}
+
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination)
+    if (distance > 2500) {
+        return "cannot travel that far";
+    } else if (distance > 2000) {
+        return 25;
+    } else if (distance > 400) {
+        return (distance - 400)*0.02;
+    } else {
+        return 0;
+    }
+}
